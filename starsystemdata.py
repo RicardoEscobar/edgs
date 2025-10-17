51851f9a74fdc99bcdaff5e8459460bb1c853410
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2025-10-17 15:32:42 -0600

    Reorder attributes in StarSystemData TypedDict for consistency

--- starsystemdata.py
+++ starsystemdata.py
@@ -4,9 +4,8 @@ from typing import TypedDict
  " database data dictionary
 """
 class StarSystemData(TypedDict):
-    id: int | None
-    StarSystem: str | None
     SystemAddress: int | None
+    StarSystem: str | None
     StarPos_x: float | None
     StarPos_y: float | None
     StarPos_z: float | None
