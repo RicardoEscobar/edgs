db6fb6a02a8186a558f4ffd2318a557015430ee6
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2025-10-17 15:09:50 -0600

    Add initial SQL database schema for EDGS application

---
+++ sql/edgs_db.sql
@@ -0,0 +1,25 @@
+/*
+EDGS SQL Database Schema
+Version: 1.0
+Author: Ricardo Escobar
+Timestamp: 2025-10-17 18:20:30 UTC
+Description: This SQL database schema is designed for the game Elite Dangerous, storing information about systems, stations, factions, and other game-related data to identify "glorious" systems and stations.
+*/
+
+-- Create the Database if it doesn't exist
+DROP DATABASE IF EXISTS edgs;
+CREATE DATABASE IF NOT EXISTS edgs;
+USE edgs;
+
+-- Table to store information about star systems
+CREATE TABLE IF NOT EXISTS starsystem (
+    id INT PRIMARY KEY,
+    StarSystem VARCHAR(255) NOT NULL,
+    SystemAddress BIGINT UNIQUE NOT NULL,
+    StarPos_x FLOAT NULL,
+    StarPos_y FLOAT NULL,
+    StarPos_z FLOAT NULL,
+    last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+SELECT * FROM edgs.starsystem;
