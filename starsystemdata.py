61a132c12f50f46b0710f16277118a9a14a1ccd3
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2025-10-19 10:52:39 -0600

    Refactor StarSystemData TypedDict to include detailed attributes for star system data

--- starsystemdata.py
+++ starsystemdata.py
@@ -4,8 +4,15 @@ from typing import TypedDict
  " database data dictionary
 """
 class StarSystemData(TypedDict):
-    SystemAddress: int | None
-    StarSystem: str | None
-    StarPos_x: float | None
-    StarPos_y: float | None
-    StarPos_z: float | None
+    id64: int | None
+    name: str | None
+    coords_x: float | None
+    coords_y: float | None
+    coords_z: float | None
+    allegiance_id: int | None
+    government_id: int | None
+    primary_economy_id: int | None
+    secondary_economy_id: int | None
+    security_id: int | None
+    population: int | None
+    date: str | None
